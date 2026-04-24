784c1c9587daf241cc7033218cb7c9ce8398bf68
Jithin <jithint1997@gmail.com>
2026-04-24 19:28:58 +0530

    feat: update Firebase configuration to use environment variables

--- services/firebase.js
+++ services/firebase.js
@@ -4,8 +4,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOT26OiILR9h1eDaiVZUazALrnPwvDLaY",
-  authDomain: "auth-demo-2026.firebaseapp.com",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
 };
 
 const app = initializeApp(firebaseConfig);
